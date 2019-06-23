da09a924ade2ff3262f97d2c93b4fc6d7d3d52a3
branko_fu <brankofulurija@gmail.com>
2019-06-23 17:03:29 +0200

    Init project

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+/**
+ * Ovde kreiramo root Vue instancu. Ona nam predstavlja sponu
+ * izmedju html-a (public/index.html) i root Vue komponente (App.vue).
+ * 
+ * Vue instanci prosledjujemo konfiguracioni objekat.
+ * Moguce konfiguracije mozete naci na https://vuejs.org/v2/api/#Options-Data
+ * 
+ * Prosledili smo komponentu koju zelimo da renderujemo nakon sto smo je importovali na liniji 2. 
+ * Komponenta ce biti renderovana na mestu HTML elementa sa id-jem id='app'.
+ */ 
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
